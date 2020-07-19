3bfbf0632a26753c69a5a80b22b76d5de8e9e3ee
LivingSaint123 <62385684+LivingSaint123@users.noreply.github.com>
2020-07-19 07:08:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+    AOS.init();
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "21em";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
